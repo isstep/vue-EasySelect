deeca701d59ad062d13da3e7afea6194c42c71da
34342343 <vzeldak07@gmail.com>
2024-09-09 23:02:22 +0300

    feat: update HeaderMain enhance responsive design with media queries

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -12,20 +12,23 @@ const props = defineProps({
   totalPrice: Number
 })
 
-let previousPrice = 0;
-watch(() => props.totalPrice, (newValue) => {
-  if (newValue > previousPrice) {
-    NumberFoods.value += 1;
-  } else if (newValue < previousPrice) {
-    NumberFoods.value -= 1;
+let previousPrice = 0
+watch(
+  () => props.totalPrice,
+  (newValue) => {
+    if (newValue > previousPrice) {
+      NumberFoods.value += 1
+    } else if (newValue < previousPrice) {
+      NumberFoods.value -= 1
+    }
+    previousPrice = newValue
   }
-  previousPrice = newValue;
-});
+)
 
 const handleScroll = () => {
   const scrollThreshold = 50
   isSticky.value = window.scrollY > scrollThreshold
-  isCollapsed.value = window.scrollY > scrollThreshold + 50
+  isCollapsed.value = window.scrollY > scrollThreshold + 10
 }
 
 const toggleCatalog = () => {
@@ -49,27 +52,27 @@ onUnmounted(() => {
 <template>
   <header
     :class="[
-      'fixed top-0 left-0 w-full bg-white border-slate-300 z-40 transition-all duration-300 ease-in-out',
+      'fixed top-0 left-0 w-full bg-white z-40 transition-all duration-300 ease-in-out',
       isSticky
-        ? isCollapsed
+      ? isCollapsed
           ? 'py-2 h-[60px] border-b'
-          : 'py-3 h-[60px] border-b'
-        : 'py-4 h-[80px] '
+          : 'py-3 h-[70px] border-b'
+        : 'py-4 h-[80px]'
     ]"
   >
-    <div class="container mx-auto flex items-center justify-between h-full px-4 md:px-8 lg:px-44">
+    <div class="flex items-center justify-between max-w-7xl mx-auto px-11">
       <router-link to="/" class="flex items-center gap-4">
-        <img src="/logo.svg" alt="logo" class="w-12" />
+        <img src="/logo.svg" alt="logo" class="w-10 h-10" />
         <div v-if="!isCollapsed" class="hidden md:block">
-          <h2 class="text-2xl font-bold">CosmoExpress</h2>
-          <p class="text-slate-400">Еда, которая прилетает к вам!</p>
+          <h2 class="text-2xl font-semibold text-gray-800">CosmoExpress</h2>
+          <p class="text-sm text-slate-500">Еда, которая прилетает к вам!</p>
         </div>
       </router-link>
 
-      <div class="flex items-center gap-4 flex-grow mx-4">
+      <div class="flex items-center gap-4 flex-grow">
         <button
           @click="toggleCatalog"
-          class="h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-full focus:shadow-outline hover:bg-indigo-800"
+          class="ml-3 h-10 px-5 text-white bg-indigo-600 hover:bg-indigo-700 rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-600 transition-colors"
         >
           Каталог
         </button>
@@ -78,31 +81,31 @@ onUnmounted(() => {
           <input
             type="text"
             placeholder="Поиск по товарам"
-            class="w-full py-2 pl-4 pr-10 border border-slate-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
-          />
+            class="w-full py-1 sm:py-2 pl-3 sm:pl-4 pr-8 sm:pr-12 text-gray-700 border border-gray-300 rounded-full shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+            />
           <span class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400">
-            <img src="/search.svg" alt="search" />
+            <img src="/search.svg" alt="search" class="w-5 h-5" />
           </span>
         </div>
       </div>
 
-      <ul class="flex items-center gap-4 md:gap-4">
+      <ul class="flex items-center gap-4">
         <li>
           <router-link
             to="/login"
-            class="flex items-center cursor-pointer gap-2 text-gray-500 hover:text-black"
+            class="flex items-center cursor-pointer gap-2 text-gray-600 hover:text-indigo-600 ml-4"
           >
-            <img class="w-5" src="/profile1.svg" alt="profile" />
+            <img class="w-5 h-5" src="/profile1.svg" alt="profile" />
             <span class="hidden md:block">Профиль</span>
           </router-link>
-        </li>
+        </li> 
 
         <li>
           <router-link
             to="/order"
-            class="flex items-center cursor-pointer gap-2 text-gray-500 hover:text-black"
+            class="flex items-center cursor-pointer gap-2 text-gray-600 hover:text-indigo-600"
           >
-            <img class="w-5" src="/order.svg" alt="order" />
+            <img class="w-5 h-5" src="/order.svg" alt="order" />
             <span class="hidden md:block">Заказы</span>
           </router-link>
         </li>
@@ -110,24 +113,23 @@ onUnmounted(() => {
         <li>
           <router-link
             to="/favorites"
-            class="flex items-center cursor-pointer gap-2 text-gray-500 hover:text-black"
+            class="flex items-center cursor-pointer gap-2 text-gray-600 hover:text-indigo-600"
           >
-            <img class="w-5" src="/heart1.svg" alt="heart" />
+            <img class="w-5 h-5" src="/heart1.svg" alt="heart" />
             <span class="hidden md:block">Избранное</span>
           </router-link>
         </li>
 
         <li
           @click="() => emit('openDrawer')"
-          class="flex items-center cursor-pointer gap-2 text-gray-500 hover:text-black"
+          class="relative flex items-center cursor-pointer gap-2 text-gray-600 hover:text-indigo-600"
         >
           <span
-            class="absolute mb-4 ml-3 w-3 h-3 inline-flex items-center justify-center mr-2 text-[0.6em] leading-none text-red-100 bg-red-600 rounded-full"
+            class="absolute mb-4 ml-3 w-3 h-3 flex items-center justify-center text-[0.6em] leading-none text-red-100 bg-red-600 rounded-full"
           >
-            {{ NumberFoods }}</span
-          >
-
-          <img class="w-5" src="/cart1.svg" alt="cart" />
+            {{ NumberFoods }}
+          </span>
+          <img class="w-5 h-5" src="/cart1.svg" alt="cart" />
           <b class="hidden md:block">{{ totalPrice }} руб.</b>
         </li>
       </ul>
