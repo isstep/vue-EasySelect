d57c68acd7b54941cd327a044348f43749b102b3
34342343 <vzeldak07@gmail.com>
2024-10-17 19:53:09 +0300

    feat: remove bg gray in SuccessPage

--- src/pages/SuccessPage.vue
+++ src/pages/SuccessPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center items-center h-screen bg-gradient-to-br from-gray-100 to-gray-300">
+  <div class="flex justify-center items-center h-screen bg-gradient-to-br">
     <div class="bg-white p-8 rounded-2xl shadow-xl">
       <h2 class="text-3xl font-bold mb-6 text-center text-gray-700">Вход выполнен успешно!</h2>
       <p class="text-center text-gray-600">Вы успешно вошли в систему.</p>
