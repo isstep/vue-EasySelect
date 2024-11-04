e902f8e5dcd148da2105778192c456d4ed840bfb
34342343 <vzeldak07@gmail.com>
2024-11-04 21:56:58 +0300

    feat: add logic in SuccesPage

--- src/pages/SuccessPage.vue
+++ src/pages/SuccessPage.vue
@@ -1,14 +1,42 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  setTimeout(() => {
+    router.push('/')
+  }, 1700)
+})
+</script>
+
 <template>
-  <div class="flex justify-center items-center h-screen bg-gradient-to-br">
-    <div class="bg-white p-8 rounded-2xl shadow-xl">
-      <h2 class="text-3xl font-bold mb-6 text-center text-gray-700">Вход выполнен успешно!</h2>
-      <p class="text-center text-gray-600">Вы успешно вошли в систему.</p>
-      <router-link 
-        to="/" 
-        class="mt-6 block text-center text-indigo-500 hover:text-indigo-600 font-medium transition duration-300"
-      >
-        Перейти на главную страницу
-      </router-link>
+  <div class="flex justify-center items-center h-[80vh] bg-gradient-to-br">
+    <div class="text-center">
+      <h1 class="text-6xl font-bold text-green-600 animate-easy-select">EasySelect</h1>
     </div>
   </div>
+  <router-link to="/"> </router-link>
 </template>
+
+<style scoped>
+@keyframes easySelectAnimation {
+  0% {
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+  50% {
+    opacity: 0.5;
+    transform: translateX(10px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+.animate-easy-select {
+  animation: easySelectAnimation 1.5s ease-in-out forwards;
+}
+</style>
