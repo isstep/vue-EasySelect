3cf3d918793f27e152c1686b8d7cd99014f8d0ef
34342343 <vzeldak07@gmail.com>
2024-11-17 00:26:43 +0300

    fix: reload confrimationpage

--- src/pages/ConfirmationPage.vue
+++ src/pages/ConfirmationPage.vue
@@ -1,14 +1,12 @@
+
 <template>
-  <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
-    <div class="bg-white shadow-md rounded-lg p-8 max-w-md w-full text-center">
-      <h2 class="text-2xl font-semibold mb-4 text-indigo-600">Подтверждение регистрации</h2>
-      <p class="text-gray-700">
-        Спасибо за регистрацию! Пожалуйста, проверьте вашу почту и подтвердите регистрацию, 
-        перейдя по ссылке в письме.
+    <div class="flex flex-col items-center justify-center h-screen">
+      <h2 class="text-2xl font-semibold mb-4">Подтверждение регистрации</h2>
+      <p class="text-gray-700 text-center">
+        Спасибо за регистрацию! Пожалуйста, проверьте вашу почту и подтвердите регистрацию, перейдя по ссылке в письме.
       </p>
     </div>
-  </div>
-</template>
+  </template>
 
 <script setup>
 import { onMounted, onBeforeUnmount } from 'vue';
